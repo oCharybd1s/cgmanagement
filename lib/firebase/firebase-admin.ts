51bb8b8526a37c44c53738d0b8f7c9bda9dc36e0
oCharybd1s <108251057+oCharybd1s@users.noreply.github.com>
2026-07-10 06:20:51 +0800

    feat(firebase): add firebase

---
+++ lib/firebase/firebase-admin.ts
@@ -0,0 +1,16 @@
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
+
+const app =
+  getApps()[0] ??
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+
+export const adminDb = getFirestore(app);
+export const adminAuth = getAuth(app);
